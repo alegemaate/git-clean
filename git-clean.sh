8457c2d29448624dc44b9e8033f5e60c85288d70
Allan Legemaate <alegemaate@gmail.com>
2021-07-22 10:15:10 -0400

    Update git-clean.sh

--- git-clean.sh
+++ git-clean.sh
@@ -3,28 +3,53 @@
 # Colors
 RED='\033[0;31m'
 YELLOW='\033[0;33m'
+GREEN='\033[0;32m'
 NC='\033[0m'
 
+# Title
+echo -e "${YELLOW} ▄▄ • ▪  ▄▄▄▄▄     ▄▄· ▄▄▌  ▄▄▄ . ▄▄▄·  ▐ ▄ ${NC}"
+echo -e "${YELLOW}▐█ ▀ ▪██ •██      ▐█ ▌▪██•  ▀▄.▀·▐█ ▀█ •█▌▐█${NC}"
+echo -e "${YELLOW}▄█ ▀█▄▐█· ▐█.▪    ██ ▄▄██▪  ▐▀▀▪▄▄█▀▀█ ▐█▐▐▌${NC}"
+echo -e "${YELLOW}▐█▄▪▐█▐█▌ ▐█▌·    ▐███▌▐█▌▐▌▐█▄▄▌▐█ ▪▐▌██▐█▌${NC}"
+echo -e "${YELLOW}·▀▀▀▀ ▀▀▀ ▀▀▀     ·▀▀▀ .▀▀▀  ▀▀▀  ▀  ▀ ▀▀ █▪${NC}"
+
 # Fetch!
-echo -e "Fetching branches";
-git fetch -p
+echo -e "${YELLOW}Fetching branches${NC}";
+git fetch --prune --quiet
 
 # Iterate
 branches=()
 
-for branch in $(git branch -vv | grep ': gone]' | awk '{print $1}'); do 
-  echo -e " ${branch}";
+for branch in $(git branch -vv | grep ': gone]' | grep -v '*' | awk '{print $1}'); do 
   branches+=($branch); 
 done;
 
+# No branches? No problem
+if [ ${#branches[@]} == 0 ]; then
+  echo -e "${GREEN}Your git is already clean!${NC}";
+  exit 0;
+fi;
+
+# There are some branches
+echo -e "${YELLOW}The following branches have been marked as 'gone' on the origin.${NC}";
+
+# Print the branches
+for branch in ${branches[*]}; do 
+  echo -e " - ${branch}";
+done;
+
 # Get response
-echo -e "${YELLOW}Are you sure you want to remove the previous branches?${NC} (y/n)";
+echo -e "${YELLOW}Would you like to remove them locally?${NC} (y/n) \c";
 
 read response;
 
 if [ "$response" = "y" ]; then
-  for branch in ${branches[*]} ; do 
-    echo -e " Deleting ${branch}";
-    git branch -D $branch;
+  for branch in ${branches[*]}; do 
+    echo -e " - Deleting ${branch}";
+    git branch -D $branch --quiet;
   done;
+
+  echo -e "${GREEN}Your branches have been cleaned!${NC}";
+else
+  echo -e "${RED}Suit yourself!${NC}";
 fi;
