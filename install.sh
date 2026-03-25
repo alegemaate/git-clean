e0395ce02774261235b0b1f407bba6b29ebddc6f
Allan Legemaate <alegemaate@gmail.com>
2026-03-25 13:20:47 -0400

    chore: adjust install config

--- install.sh
+++ install.sh
@@ -1,8 +1,30 @@
 #!/bin/bash
 
-echo -e "Installing to /usr/local/bin"
+set -e
 
-cp git-clean.sh /usr/local/bin/git-clean
-chmod +x /usr/local/bin/git-clean
+# Determine install directory
+if [ "$(uname)" = "Darwin" ]; then
+  INSTALL_DIR="/usr/local/bin"
+elif [ "$(uname)" = "Linux" ]; then
+  if [ -d "$HOME/.local/bin" ]; then
+    INSTALL_DIR="$HOME/.local/bin"
+  else
+    INSTALL_DIR="/usr/local/bin"
+  fi
+else
+  echo "Unsupported OS: $(uname)"
+  exit 1
+fi
 
-echo -e "Done!"
+echo "Installing to $INSTALL_DIR"
+
+# Use sudo if installing to a system directory and not running as root
+if [ "$EUID" -ne 0 ] && [[ "$INSTALL_DIR" == /usr/* ]]; then
+  sudo cp git-clean.sh "$INSTALL_DIR/git-clean"
+  sudo chmod +x "$INSTALL_DIR/git-clean"
+else
+  cp git-clean.sh "$INSTALL_DIR/git-clean"
+  chmod +x "$INSTALL_DIR/git-clean"
+fi
+
+echo "Done!"
